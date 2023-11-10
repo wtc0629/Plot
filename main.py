dc885b5d897ae15e135d414d39c1e5644f0dc050
wtc0629 <wtc19990629@gmail.com>
2023-11-10 06:39:09 +0100

    2

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ gestures = ["horizontally", "horizontally fast", "vertically", "vertically fast"
 
 # print(test)
 
+
+
 def save_multi_image(filename):
     pp = PdfPages(filename)
     fig_nums = plt.get_fignums()
@@ -18,27 +20,26 @@ def save_multi_image(filename):
         fig.savefig(pp, format='pdf')
     pp.close()
 
-newtest2 = test.loc[test['process'] == "horizontally_GestureTime0"]
-print(newtest2)
 
+colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
 for j in gestures:
     nowGestures = ''
     newtest = []
     for i in range(8):
         nowGestures = j + '_GestureTime' + str(i)
         newtest = test.loc[test['process'] == nowGestures]
-
         if j == 'near to far' or j == 'near to far fast':
             newtest.plot(x='gaze_point_3d_y', y='gaze_point_3d_z', title=j + ' y-z ' + str(i))
-            #newtest.quiver(x='gaze_point_3d_y', y='gaze_point_3d_z',u=testdiff['gaze_point_3d_y'], v=testdiff['gaze_point_3d_z'])
-            #newtest.plot(x='gaze_point_3d_x', y='gaze_point_3d_z', title=j + ' x-z ' + str(i))
+         #  newtest.plot(x='gaze_point_3d_x', y='gaze_point_3d_z', title=j + ' x-z ' + str(i))
         else:
             newtest.plot(x='gaze_point_3d_x', y='gaze_point_3d_y', title=j + ' x-y ' + str(i))
-            #newtest.quiver(x='gaze_point_3d_x', y='gaze_point_3d_y', title=j + ' x-y ' + str(i))
+
 
     filename = 'C:\\Users\\51004\\Desktop\\MergeCSV\\Dennis\\' + j + '.pdf'
     save_multi_image(filename)
     plt.close('all')
+
+
 
     # plt.savefig('C:\\Users\\51004\\Desktop\\MergeCSV\\Dennis\\'+j+str(i)+'.png')
     # plt.close()
